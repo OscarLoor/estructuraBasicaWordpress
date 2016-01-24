70fba2594d2595b48ef3bb963698309c7ab1d500
Oscar Loor <dwquito@hotmail.com>
2016-01-23 21:01:43 -0500

    Creación del footer
    Creación del footer y agrego los widgets también es responsive

--- footer.php
+++ footer.php
@@ -1,9 +1,47 @@
 <?php wp_footer(); ?>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="<?php echo get_template_directory_uri();?>/js/bootstrap.min.js"></script>
 
-</body>
+ <div id="footer" class="row">
+  <div id="parteSuperiorFooter" class="container">
+   <div class="col-md-3">
+    <a id="logoFooter" href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/img/logoNegro.png"></a>
+   </div><!--col-md-3-->
+   <div class="col-md-3">
+    <?php if ( is_active_sidebar( 'footer-uno' ) ) : ?>
+     <?php dynamic_sidebar( 'footer-uno' ); ?>
+    <?php endif; ?>
+   </div><!--col-md-3-->
+   
+   <div class="col-md-3">
+    <?php if ( is_active_sidebar( 'footer-dos' ) ) : ?>
+     <?php dynamic_sidebar( 'footer-dos' ); ?>
+    <?php endif; ?>
+   </div><!--col-md-3-->
+   
+   <div id="datosContacto" class="col-md-3">
+    <?php if ( is_active_sidebar( 'footer-tres' ) ) : ?>
+     <?php dynamic_sidebar( 'footer-tres' ); ?>
+    <?php endif; ?>
+   </div><!--col-md-3-->
+  </div>
+  <div id="parteInferiorFooter" class="container">
+   <p><span>Todos los derechos reservados ©2016 DEOFERTA.EC</span></p>
+   <p><a href="http://www.paginaswebenquito.ec/" target="_blank">PáginasWebEnQuito.EC</a> |
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Tienda Virtual</a> -
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Diseño Web</a> -
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Desarrollo Web</a> -
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Sitios Web</a> -
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Paginas Web</a> -
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Páginas Web</a> -
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Revistas Digitales</a> -
+    <a href="http://www.paginaswebenquito.ec/" target="_blank">Catálogos Digitales</a></p>
+  </div>
+ </div>
 
-</html>
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ <!-- Include all compiled plugins (below), or include individual files as needed -->
+ <script src="<?php echo get_template_directory_uri();?>/js/bootstrap.min.js"></script>
+
+ </body>
+
+ </html>
--- functions.php
+++ functions.php
@@ -10,4 +10,35 @@ function registrarMenuSuperior() {
 }
 add_action( 'init', 'registrarMenuSuperior' );
 
+function registrarSideBars(){
+		register_sidebar( array(
+			'name'          => 'Footer Uno',
+			'id'            => 'footer-uno',
+			'description'   => 'Aparece en la parte inferior de la página',
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '<h3>',
+			'after_title'   => '</h3>',
+		) );
+ 
+ register_sidebar( array(
+			'name'          => 'Footer Dos',
+			'id'            => 'footer-dos',
+			'description'   => 'Aparece en la parte inferior de la página',
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '<h3>',
+			'after_title'   => '</h3>',
+		) );
+ register_sidebar( array(
+			'name'          => 'Footer Tres',
+			'id'            => 'footer-tres',
+			'description'   => 'Aparece en la parte inferior de la página',
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '<h3>',
+			'after_title'   => '</h3>',
+		) );
+	}
+add_action( 'widgets_init', 'registrarSideBars' );
 ?>
