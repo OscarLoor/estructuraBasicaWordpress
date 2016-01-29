c84cdcf5f4d6fbce0bd21c8c06f9425fd67406a7
Oscar Loor <dwquito@hotmail.com>
2016-01-29 02:04:42 -0500

    Redes sociales al lado derecho y css adicional para responsive
    Redes sociales al lado derecho y css adicional para responsive

--- footer.php
+++ footer.php
@@ -1,27 +1,45 @@
 <?php wp_footer(); ?>
+ <div class="social-icons">
+  <a class="socialitems" target="_blank" href="https://es-la.facebook.com/deofertaec/">
+   <p>
+    <i class="fa fa-facebook-official"></i>
+    <span class="social-text">Facebook</span>
+   </p>
+  </a>
+ </div>
+ <div id="redSocialAbajo" class="social-icons">
+   <p class="socialitems">
+    <i class="fa fa-whatsapp"></i>
+    <span class="social-text">Whatsapp - 0980 860 534</span>
+   </p>
+ </div>
 
  <div id="footer" class="row">
   <div id="parteSuperiorFooter" class="container">
    <div class="col-md-3">
     <a id="logoFooter" href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/img/logoNegro.png"></a>
-   </div><!--col-md-3-->
+   </div>
+   <!--col-md-3-->
    <div class="col-md-3">
     <?php if ( is_active_sidebar( 'footer-uno' ) ) : ?>
      <?php dynamic_sidebar( 'footer-uno' ); ?>
-    <?php endif; ?>
-   </div><!--col-md-3-->
-   
+      <?php endif; ?>
+   </div>
+   <!--col-md-3-->
+
    <div class="col-md-3">
     <?php if ( is_active_sidebar( 'footer-dos' ) ) : ?>
      <?php dynamic_sidebar( 'footer-dos' ); ?>
-    <?php endif; ?>
-   </div><!--col-md-3-->
-   
+      <?php endif; ?>
+   </div>
+   <!--col-md-3-->
+
    <div id="datosContacto" class="col-md-3">
     <?php if ( is_active_sidebar( 'footer-tres' ) ) : ?>
      <?php dynamic_sidebar( 'footer-tres' ); ?>
-    <?php endif; ?>
-   </div><!--col-md-3-->
+      <?php endif; ?>
+   </div>
+   <!--col-md-3-->
   </div>
   <div id="parteInferiorFooter" class="container">
    <p><span>Todos los derechos reservados ©2016 DEOFERTA.EC</span></p>
